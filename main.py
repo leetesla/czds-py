110cecd4075947f5b58a8aa7cd061d6713511713
chrise <leetesla@sina.com>
2025-11-06 19:54:30 +0800

    MOD:lm

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 
 # 检查是否启用了低内存模式
-if "--low-memory" in sys.argv:
+if "--lm" in sys.argv:
     from scripts.run import run_task_low_memory
     if __name__ == "__main__":
         run_task_low_memory()
