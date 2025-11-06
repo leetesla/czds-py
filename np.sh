c72422f7108bdb22da3338a3d428f49f8d25bbb3
chrise <leetesla@sina.com>
2025-11-06 18:06:58 +0800

    MOD:np

--- np.sh
+++ np.sh
@@ -1,2 +1,2 @@
 #nohup /Users/chrise/miniconda3/envs/quant/bin/python main.py > nohup.log 2>&1 &
-nohup /home/ubuntu/miniconda3/bin/python download.py > lnp.log 2>&1 &
+nohup uv run main.py > app.log 2>&1 &
