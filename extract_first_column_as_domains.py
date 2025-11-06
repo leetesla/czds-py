ed142af257e450cba1a8c97c3679a3c2a6f91c06
chrise <leetesla@sina.com>
2025-11-06 12:10:04 +0800

    MOD:整理代码

---
+++ extract_first_column_as_domains.py
@@ -0,0 +1,10 @@
+from app_config.constant import DIR_OUTPUT_DOMAINS_001, DIR_DOWNLOAD_001, DIR_OUTPUT_DOMAINS_002, DIR_DOWNLOAD_ZONEFILES
+from scripts.extract_first_column import extract_first_column_from_directory
+
+if __name__ == "__main__":
+
+    # print("********* extract_first_column_from_directory() ********")
+    # extract_first_column_from_directory(DIR_DOWNLOAD_001, DIR_OUTPUT_DOMAINS_001)
+
+    print("********* extract_first_column_from_directory() ********")
+    extract_first_column_from_directory(DIR_DOWNLOAD_ZONEFILES, DIR_OUTPUT_DOMAINS_002)
